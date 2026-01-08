bcab5f62e180dd6b6f6b3176f57c99c5c2d005ed
OMG <93129992+KONGCHAN-999@users.noreply.github.com>
2026-01-08 09:05:26 +0700

    update8/1/2026/v2

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,22 +6,8 @@ import Views from '@/views'
 import appConfig from './configs/app.config'
 import './locales'
 
-<<<<<<< HEAD
 if (appConfig.enableMock) {
     import('./mock')
-=======
-import viteLogo from "/vite.svg";
-import Attendance from "./Attendance/Attendance";
-
-function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <Attendance/>
-    </>
-  );
->>>>>>> 638286203ab710108e5caa66f86f4a40affa62f5
 }
 
 function App() {
